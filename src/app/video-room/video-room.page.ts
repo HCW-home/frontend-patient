f294f052b7ad19669df20d418aa571906f3ebf81
Muhammed <aapozaid@gmail.com>
2022-04-04 12:57:37 +0200

    FIX issues with video toggle during call refs #4199

--- src/app/video-room/video-room.page.ts
+++ src/app/video-room/video-room.page.ts
@@ -224,29 +224,14 @@ export class VideoRoomPage implements OnInit, OnDestroy {
 
   ngOnInit() {
     console.log("Initialize video", this.token, this.audioOnly);
-
+    if (this.message.type === "audioCall") {
+      this.camStatus = "off";
+    }
     this.peerId = this.authService.currentUserValue.id;
 
     if (this.accepted) {
       this.joinToSession();
     }
-
-    // this.accepted = !this.token;
-
-    // this.subscriptions.push(this.socketSer.consultationClosedSubj.subscribe(consultation => {
-    //   if (consultation._id === this.sessionId && consultation.consultation && consultation.consultation.status === 'closed') {
-    //     console.log('consultation closed >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..')
-    //     this.exitSession();
-    //     return true;
-    //   }
-    // }));
-
-    // this.subscriptions.push(this.socketSer.onRejectCall().subscribe(event => {
-    //   console.log('call rejected ', event);
-    //   // if (event.data.consultation.id === this.sessionId) {
-    //   //   this.exitSession();
-    //   // }
-    // }));
   }
 
   ngOnDestroy() {
@@ -367,7 +352,6 @@ export class VideoRoomPage implements OnInit, OnDestroy {
     } else {
       this.rejectCall();
     }
-    // this.router.navigate(['']);
   }
 
   resetVideoSize() {
