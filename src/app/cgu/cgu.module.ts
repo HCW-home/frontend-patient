c299d0e642ed2df232f567866b68fcb8a70b127b
Olivier Bitsch <olivier.b@iabsis.com>
2022-04-07 11:51:42 +0200

    Adding the possibility to customize terms and policies with Markdown file

--- src/app/cgu/cgu.module.ts
+++ src/app/cgu/cgu.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { MarkdownModule } from "ngx-markdown";
 
 import { IonicModule } from '@ionic/angular';
 
@@ -23,7 +24,8 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
-    I18nModule
+    I18nModule,
+    MarkdownModule.forRoot()
   ],
   declarations: [CguPage]
 })
