45d1b1cdf903435f5411a1ac863398b8bfb18d91
Olivier Bitsch <olivier.b@iabsis.com>
2022-09-22 15:45:21 +0200

    After remove cordova

---
+++ capacitor.config.ts
@@ -0,0 +1,17 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hug_at_home',
+  appName: 'hug_at_home',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      'android-targetSdkVersion': '31',
+      BackupWebStorage: 'none'
+    }
+  }
+};
+
+export default config;
