26b068e3490befad4f85ead22c499ebc5746b3df
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2023-08-07 13:28:10 +0400

    environment cleanup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,6 @@ export const environment = {
   production: true,
   api: '/api/v1',
   host: 'https://' + document.location.host,
-  //host:  "https://patient_hcw-athome.dev.oniabsis.com",
-  //host: "http://localhost:1337",
-
   DN: '',
   showNativeAppSuggestionAndroid: false,
   showNativeAppSuggestionIOS: false,
