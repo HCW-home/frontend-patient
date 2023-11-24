12bd8ac41ec6d30d239dcd7d42120d415a07b993
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2023-11-24 14:50:33 +0400

    remve usehash logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,7 +38,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
