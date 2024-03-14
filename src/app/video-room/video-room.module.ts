630e22686df950c8dae64f33acb9a91f7fba314c
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2024-03-14 17:01:06 +0400

    build fix

--- src/app/video-room/video-room.module.ts
+++ src/app/video-room/video-room.module.ts
@@ -3,15 +3,12 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
-// translate
 import { I18nModule } from '../i18n/i18n.module';
 
 import { VideoRoomPage } from './video-room.page';
-// import { StreamComponent } from '../shared/components/stream/stream.component';
-// import { OpenViduVideoComponent } from '../shared/components/stream/ov-video.component';
 import { ChatComponent } from '../shared/components/chat/chat.component';
 
 import { AngularDraggableModule } from 'angular2-draggable';
@@ -30,7 +27,8 @@ const routes: Routes = [
         IonicModule,
         AngularDraggableModule,
         I18nModule,
-        SharedModule
+        SharedModule,
+        AngularDraggableModule,
         // RouterModule.forChild(routes),
     ],
     declarations: [VideoRoomPage, ChatComponent,],
